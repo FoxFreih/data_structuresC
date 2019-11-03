f3c594d6029e303c851ebd726c7470ea5fdd2e14
Amal Saleh aboFreih <55637208+FoxFreih@users.noreply.github.com>
2019-11-03 15:17:31 +0200

    linked list in c

---
+++ linkedList.c
@@ -0,0 +1,89 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<string.h>
+
+struct Node
+{
+	int data;
+	struct Node* next;
+};
+struct Node* head;
+struct Node* tail;
+struct Node* insert(int element)
+{
+	struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+	temp->data = element;
+	temp->next = head;
+	head = temp;
+	return head;
+}
+struct Node* insert_to_tail(int element)
+{
+	struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+	if (head == NULL)
+	{
+		temp->data = element;
+		temp->next = tail;
+		head = temp;
+	}
+	else
+	{
+		temp->data = element;
+		temp->next = NULL;
+		tail->next = temp;
+		tail = tail->next;
+	}
+
+
+	return tail;
+	
+}
+
+void print()
+{
+	struct Node* temp = head;
+	while (temp != NULL)
+	{
+		printf("%d -> ", temp->data);
+		temp = temp->next;
+	}
+	printf("NULL \n");
+
+}
+
+
+void main()
+{
+	int size, element;
+	head = NULL;
+	tail = head;
+	printf("how many nodes to insert ? ");
+	scanf("%d", &size);
+	for (int i = 0; i < size; i++)
+	{
+		printf("enter element %d : ", i + 1);
+		scanf("%d", &element);
+		if (head == NULL)
+		{
+			tail = NULL;
+			insert_to_tail(element);
+		}
+		else
+		{
+			struct Node* temp = head;
+			while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		tail = temp;
+		insert_to_tail(element);
+
+		}
+	
+	}
+	//printf("%d", tail->data);
+	print();
+	
+
+	return 0;
+}
